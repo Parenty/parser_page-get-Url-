fc94bc584667577787fb4c4578a3126b034db123
Parenty <43603737+Parenty@users.noreply.github.com>
2019-06-20 08:47:00 +0300

    токен теперь вычисляется при отправке запроса на страницу

--- Parser_script.py
+++ Parser_script.py
@@ -7,6 +7,7 @@ import time
 
 
 start_time = time.time()
+
 def get_html_and_stcode(url):
 	r = requests.get(url)
 	a = [r.text, r.status_code]
@@ -14,13 +15,29 @@ def get_html_and_stcode(url):
 			#возвращает html-код страницы и статус код
 
 
-#def loginbot()
-	#s = requests.Session()
-	#s = requests.get(https://uchi.ru/)
-	#data = ['backurl']
-	
+def loginbot():
+	s = requests.Session()
+	resp = s.get("https://43059.shot-uchi.ru/")
+	html_text = resp.text
+	token = re.search('(?<=\<meta name=\"csrf-token\" content=\")(.+)(?=\" />)', html_text)
+	print(s.cookies.get_dict())
+	#print(token)
 
 
+	data = {#"utf8": '%E2%9C%93',
+			"authenticity_token": token,
+			#"next": '/home',
+			"login": 'dmitriev@uchi.ru',
+			"password": '123'
+	 }
+
+	r = s.post('https://43059.shot-uchi.ru/', data = data)
+
+	#print(r.text)
+	print(r.status_code)
+
+
+"""
 def get_html(page_andst):
 	text_html = page_andst[0]
 	return text_html
@@ -34,8 +51,10 @@ def get_status_code(page_andst):
 def get_only_url(html_page):
 	only_url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', html_page)
 	return only_url
-
+"""
 def main():
+	loginbot()
+	"""
 	url = 'https://httpbin.org/'
 	main_page_list = get_html_and_stcode(url) #код страницы и статус-код
 	main_page = get_html(main_page_list) #код страницы
@@ -64,7 +83,7 @@ def main():
 
 
 
-
+"""
 if __name__ == '__main__':
 	main()
 
