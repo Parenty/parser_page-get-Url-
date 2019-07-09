a40f3d9e09855f153bc3f2d99e2558df71677bae
Parenty <43603737+Parenty@users.noreply.github.com>
2019-07-09 17:48:27 +0300

    Сделал без рекурсии (теперь забираю абсолютно все ссылки)

--- Parser_script.py
+++ Parser_script.py
@@ -14,37 +14,56 @@ Host = 'https://43059.shot-uchi.ru'
 Teacher_first_page = '/teachers/stats/main'
 links = set()
 count = 0
+count_while = 0
+links_while = []
+
 list_of_stcode = []
-
-check_list = []
-check_url = []
-
-
+final_list = []
+# check_list = []
+# check_url = []
 
 
-def post_html_and_stcode(url):
+s = requests.Session()
+resp = s.get(Host)
+html_text = resp.text
+token = (re.search('(?<=\<meta name=\"csrf-token\" content=\")(.+)(?=\" />)', html_text)).group()
 
-	s = requests.Session()
-	resp = s.get(url)
-	html_text = resp.text
-	token = (re.search('(?<=\<meta name=\"csrf-token\" content=\")(.+)(?=\" />)', html_text)).group()
-
-
-	data = {#"utf8": '%E2%9C%93',
+data = {#"utf8": '%E2%9C%93',
 			"authenticity_token": token,
 			#"next": '/home',
 			"login": 'dmitriev@uchi.ru',
 			"password": '1'
 	 }
 
-	r = s.post(url, data = data)
+r = s.post(Host, data = data)
 
-	return s
 
+
+# def post_html_and_stcode(url):
+
+# 	s = requests.Session()
+# 	resp = s.get(url)
+# 	html_text = resp.text
+# 	token = (re.search('(?<=\<meta name=\"csrf-token\" content=\")(.+)(?=\" />)', html_text)).group()
+
+
+# 	data = {#"utf8": '%E2%9C%93',
+# 			"authenticity_token": token,
+# 			#"next": '/home',
+# 			"login": 'dmitriev@uchi.ru',
+# 			"password": '1'
+# 	 }
+
+# 	r = s.post(url, data = data)
+
+	# return s
+
+# s = post_html_and_stcode(Host)
 def get_html_and_stcode(url):
-	r = post_html_and_stcode(Host).get(url, timeout = 5)
-	# r = requests.get(url)
-	a = [r.text, r.status_code]
+	r2 = s.get(url, timeout = 30)
+	# r = post_html_and_stcode(Host).get(url, timeout = 5)
+	# r = requests.get(url, timeout = 5)
+	a = [r2.text, r2.status_code]
 	return a	
 			#возвращает html-код страницы и статус код
 
@@ -60,7 +79,7 @@ def get_status_code(page_andst):
 
 def get_only_url(html_page):
 	soup = BeautifulSoup(html_page, 'lxml') #создаю объект супа
-	#list_urls = []
+	list_urls = []
 	for i in soup.find_all('link', href = True): #ищу все ссылки с тэгом link
 		link = str(i.get('href'))
 		list_urls.append(link)
@@ -69,28 +88,91 @@ def get_only_url(html_page):
 		link = str(i.get('href'))
 		list_urls.append(link)
 
-		#достаю остальные ссылки (начинающиеся с http)
+		# достаю остальные ссылки (начинающиеся с http)
+	only_url = re.findall('((?<=href=")([\/].+)(?= [\/]))', html_page)
 	only_url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', html_page)
 	for i in only_url:
 		list_urls.append(i)
 
-	for i in list_urls:
-		if not i.startswith('/'):
-			list_urls.remove(i)
+	# for i in list_urls:
+	# 	if not i.startswith('/'):
+	# 		list_urls.remove(i)
 
 
-	unique_urls = set(list_urls)
+	# unique_urls = set(list_urls)
 
-	return unique_urls
+	return list_urls
 
 
 
-def get_all_links(url, maxdepth = 2): #maxdepth - глубина рекурсии. Для учи, рекомендую ставить "2" - основные урлы будут проверены
+
+def get_links_while(url):
+
+	links_while = [url]
+
+	global count_while
+
+	# while count_while != 1:                   #len(links_while)
+
+	for i in links_while:
+
+		# count_while = count_while + 1
+
+		if count_while == len(links_while):
+
+			req = get_html_and_stcode(i)
+			req_html = get_html(req)
+			req_st_code = get_status_code(req)
+
+			links_and_response = {"url": i, "status_code": req_st_code}
+			final_list.append(links_and_response)
+
+			print(links_and_response)
+			print('\n')
+			print(count_while)
+
+			print("Все ссылки пройдены")
+
+			return
+
+		else:
+
+			count_while = count_while + 1
+
+			req = get_html_and_stcode(i)
+			req_html = get_html(req)
+			req_st_code = get_status_code(req)
+
+			urls_while = get_only_url(req_html)
+
+			links_and_response = {"url": i, "status_code": req_st_code}
+			final_list.append(links_and_response)
+
+			print(links_and_response)
+			print('\n')
+			print(count_while)
+
+			for link in urls_while:
+
+				if not '/logout' in link:
+
+					if link.startswith('/') and not link.startswith('//'):
+						link = Host + link
+
+					if link.startswith(Host) and link not in links_while:
+						links_while.append(link)
+
+
+
+
+
+
+def get_all_links(url, maxdepth = 4): #maxdepth - глубина рекурсии. Для учи, рекомендую ставить "3" - основные урлы будут проверены
 
 	global count
 	
 	links_recursive = []
-	# post_html_and_stcode(Host)
+	# s = post_html_and_stcode(Host)
 	request = get_html_and_stcode(url)
 	request_html = get_html(request)
 	request_st_code = get_status_code(request)
@@ -113,16 +195,18 @@ def get_all_links(url, maxdepth = 2): #maxdepth - глубина рекурси�
 
 	# print(links_recursive)
 	for link in urls:
-		if link.startswith('/') and not link.startswith('//'):
-			link = Host + link
-		if link.startswith(Host) and link not in links and not link in links_recursive:
-			# print(link)
-			links.add(link)
-			links_recursive.append(link)
-			# print(link)
-			# print(str(len(links))+' links')
-			# print(str(len(links_recursive))+' links_recursive')
-			# print('\n')
+		if not '/logout' in link:
+
+			if link.startswith('/') and not link.startswith('//'):
+				link = Host + link
+			if link.startswith(Host) and link not in links and not link in links_recursive:
+				# print(link)
+				links.add(link)
+				links_recursive.append(link)
+				# print(link)
+				# print(str(len(links))+' links')
+				# print(str(len(links_recursive))+' links_recursive')
+				# print('\n')
 	# print(str(len(links))+' links')
 	# print(str(len(links_recursive))+' links_recursive')
 	if maxdepth > 0:
@@ -137,11 +221,16 @@ def get_all_links(url, maxdepth = 2): #maxdepth - глубина рекурси�
 
 def main():
 	global count
-	get_all_links(Host+Teacher_first_page)
-	print(len(check_url))
+	get_links_while(Host+Teacher_first_page)
+	# get_all_links(Host+Teacher_first_page)
+	# print(len(check_url))
+
+	# with open("statuscode.json", "w", encoding = "utf-8") as file:
+	# 	json.dump(list_of_stcode, file, indent = 4)
+	# 	file.close()
 
 	with open("statuscode.json", "w", encoding = "utf-8") as file:
-		json.dump(list_of_stcode, file, indent = 4)
+		json.dump(final_list, file, indent = 4)
 		file.close()
 
 			
@@ -150,7 +239,7 @@ def main():
 	# 	print(i)
 	# for link in links:
 	# 	print(link)
-	print(len(links))
+	# print(len(links))
 	# print(links)
 	# for link in links:
 	# 	count = count + 1
